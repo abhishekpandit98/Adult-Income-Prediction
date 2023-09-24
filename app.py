a3bdd8c39245de1dd5589016a10f00ed6889391f
ABHISHEK PANDIT <abhishekpandit1998@gmail.com>
2023-09-24 18:10:13 +0530

    saved preprocessing object

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-import sys
-import os
-import pandas as pd
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
- 
+
+import sys
+
+
 if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        ##loading train and test data to perform data transformation
-        train_data = pd.read_csv(os.path.join('artifacts', 'train_df.csv'))
-        test_data = pd.read_csv(os.path.join('artifacts', 'test_df.csv'))
-        logging.info("loading train and test data done")
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        ##performing data transformation
-        data_transformation_config = DataTransformationConfig()
-        data_transformation = DataTransformation()
-        train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data)
-        logging.info("performing data transformation done")
-
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        
     except Exception as e:
-        logging.error("Custom Exception")
-        raise CustomException(e, sys)
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+
--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -5,12 +5,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 import sys
+from dataclasses import dataclass
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import os
 from src.mlproject.utils import save_object
 
-from dataclasses import dataclass
 
 @dataclass
 class DataTransformationConfig:
@@ -18,9 +18,12 @@ class DataTransformationConfig:
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformationConfig()
+        self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
+        '''
+        this function is responsible for data transformation
+        '''
         try:
             numeric_features = ['age', 'education-num', 'hours-per-week']
             categorical_features = ['workclass', 'education', 'marital-status', 'occupation', 'race', 'sex', 'country']
@@ -40,7 +43,8 @@ class DataTransformation:
             preprocessor = ColumnTransformer([
                 ("numerical_pipeline", num_pipeline, numeric_features),
                 ("categorical_pipeline", cat_pipeline, categorical_features)
-            ])
+            ]
+            )
 
             return preprocessor
         except Exception as e:
@@ -48,39 +52,52 @@ class DataTransformation:
 
     def initiate_data_transformation(self, train_path, test_path):
         try:
-            train_df = train_path
-            test_df = test_path
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
             logging.info("Reading train and test file")
 
             preprocessing_obj = self.get_data_transformer_object()
-            target_column = 'salary'
-            input_features_train_df = train_df.drop(target_column, axis=1)
-            target_features_train_df = train_df[target_column]
+            target_column_name = 'salary'
 
-            input_features_test_df = test_df.drop(target_column, axis=1)
-            target_features_test_df = test_df[target_column]
+            ## divide the train dataset to independent and dependent feature
+            input_features_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+
+            ## divide the test dataset to independent and dependent feature
+
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
             logging.info("Applying preprocessing on train and test dataframe")
 
-            input_features_train_arr = preprocessing_obj.fit_transform(input_features_train_df)
-            input_features_test_arr = preprocessing_obj.transform(input_features_test_df)
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_features_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            print("Shapes:")
-            print("input_features_train_arr shape:", input_features_train_arr.shape)
-            print("input_features_test_arr shape:", input_features_test_arr.shape)
-            print("target_features_train_df shape:", target_features_train_df.shape)
-            print("target_features_test_df shape:", target_features_test_df.shape)
+                        # Check the shapes again
+            print("Shapes after reshaping:")
+            print("input_features_train_arr shape:", input_feature_train_arr.shape)
+            print("input_features_test_arr shape:", input_feature_test_arr.shape)
+            print("target_features_train_arr shape:", np.array(target_feature_train_df).shape)
+            print("target_features_test_arr shape:", np.array(target_feature_test_df).shape)
 
-            train_arr = np.c_[input_features_train_arr, np.array(target_features_train_df)]
-            test_arr = np.c_[input_features_test_arr, np.array(target_features_test_df)]
+            
+            train_arr = np.hstack((input_feature_train_arr, target_feature_train_df))
+            test_arr =  np.hstack((input_feature_test_arr, target_feature_test_df))
 
-            logging.info("Saved preprocessing object")
+            logging.info(f"Saved preprocessing object")
+
+            logging.info(f"Saved preprocessing object")
 
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
             )
+            
 
-            return train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
+            return(train_arr,
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file_path)
         except Exception as e:
             raise CustomException(e, sys)
