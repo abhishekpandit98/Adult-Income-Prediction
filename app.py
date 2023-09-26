8d1474901d4a2fa045ace22c1820f8562ea1279b
ABHISHEK PANDIT <abhishekpandit1998@gmail.com>
2023-09-26 18:58:00 +0530

    Streamlit UI imrpoved

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
 
     try:
         # data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        # data_ingestion=DataIngestion()
+        # train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         # data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,preprocessor_file_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        # data_transformation=DataTransformation()
+        # train_arr,test_arr,preprocessor_file_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
         # Model Training
         # model_trainer=ModelTrainer()
@@ -28,39 +28,44 @@ if __name__ == "__main__":
         # Initialize Streamlit UI
         logging.info("The Streamlit execution has started")
 
-        st.title("Machine Learning Prediction App")
+        st.title("Adult Income Prediction App")
         st.write("Enter the data for prediction:")
+        container = st.container() 
+        with container:
+         col1, col2 = st.columns(2)
 
         # Create input fields for user input
-        age = st.number_input("Age", min_value=0, max_value=100)
-        workclass = st.selectbox("Workclass", [' State-gov', ' Self-emp-not-inc', ' Private', ' Federal-gov',
-                                       ' Local-gov', ' Self-emp-inc', ' Without-pay',
-                                       ' Never-worked'])
-        education = st.selectbox("Education", [' Bachelors', ' HS-grad', ' 11th', ' Masters', ' 9th',
-                                       ' Some-college', ' Assoc-acdm', ' Assoc-voc', ' 7th-8th',
-                                       ' Doctorate', ' Prof-school', ' 5th-6th', ' 10th', ' 1st-4th',
-                                       ' Preschool', ' 12th'])
-        education_num = st.number_input("Education Number", min_value=0, max_value=20)
-        marital_status = st.selectbox("Marital Status", [' Never-married', ' Married-civ-spouse', ' Divorced',
-                                                 ' Married-spouse-absent', ' Separated', ' Married-AF-spouse',
-                                                 ' Widowed'])
-        occupation = st.selectbox("Occupation", [' Adm-clerical', ' Exec-managerial', ' Handlers-cleaners',
-                                         ' Prof-specialty', ' Other-service', ' Sales', ' Craft-repair',
-                                         ' Transport-moving', ' Farming-fishing', ' Machine-op-inspct',
-                                         ' Tech-support', ' Protective-serv', ' Armed-Forces',
-                                         ' Priv-house-serv'])
-        race = st.selectbox("Race", [' White', ' Black', ' Asian-Pac-Islander', ' Amer-Indian-Eskimo', ' Other'])
-        sex = st.selectbox("Sex", [' Male', ' Female'])
-        hours_per_week = st.number_input("Hours Per Week", min_value=0, max_value=168)
-        country = st.selectbox("Country", [' United-States', ' Cuba', ' Jamaica', ' India', ' Mexico',
-                                    ' South', ' Puerto-Rico', ' Honduras', ' England', ' Canada',
-                                    ' Germany', ' Iran', ' Philippines', ' Italy', ' Poland',
-                                    ' Columbia', ' Cambodia', ' Thailand', ' Ecuador', ' Laos',
-                                    ' Taiwan', ' Haiti', ' Portugal', ' Dominican-Republic',
-                                    ' El-Salvador', ' France', ' Guatemala', ' China', ' Japan',
-                                    ' Yugoslavia', ' Peru', ' Outlying-US(Guam-USVI-etc)', ' Scotland',
-                                    ' Trinadad&Tobago', ' Greece', ' Nicaragua', ' Vietnam', ' Hong',
-                                    ' Ireland', ' Hungary', ' Holand-Netherlands'])
+        with col1:
+            age = st.number_input("Age", min_value=0, max_value=100)
+            workclass = st.selectbox("Workclass", [' State-gov', ' Self-emp-not-inc', ' Private', ' Federal-gov',
+                                        ' Local-gov', ' Self-emp-inc', ' Without-pay',
+                                        ' Never-worked'])
+            education = st.selectbox("Education", [' Bachelors', ' HS-grad', ' 11th', ' Masters', ' 9th',
+                                        ' Some-college', ' Assoc-acdm', ' Assoc-voc', ' 7th-8th',
+                                        ' Doctorate', ' Prof-school', ' 5th-6th', ' 10th', ' 1st-4th',
+                                        ' Preschool', ' 12th'])
+            education_num = st.number_input("Education Number", min_value=0, max_value=20)
+            marital_status = st.selectbox("Marital Status", [' Never-married', ' Married-civ-spouse', ' Divorced',
+                                                    ' Married-spouse-absent', ' Separated', ' Married-AF-spouse',
+                                                  ' Widowed'])
+        with col2:      
+            occupation = st.selectbox("Occupation", [' Adm-clerical', ' Exec-managerial', ' Handlers-cleaners',
+                                            ' Prof-specialty', ' Other-service', ' Sales', ' Craft-repair',
+                                            ' Transport-moving', ' Farming-fishing', ' Machine-op-inspct',
+                                            ' Tech-support', ' Protective-serv', ' Armed-Forces',
+                                            ' Priv-house-serv'])
+            race = st.selectbox("Race", [' White', ' Black', ' Asian-Pac-Islander', ' Amer-Indian-Eskimo', ' Other'])
+            sex = st.selectbox("Sex", [' Male', ' Female'])
+            hours_per_week = st.number_input("Hours Per Week", min_value=0, max_value=168)
+            country = st.selectbox("Country", [' United-States', ' Cuba', ' Jamaica', ' India', ' Mexico',
+                                        ' South', ' Puerto-Rico', ' Honduras', ' England', ' Canada',
+                                        ' Germany', ' Iran', ' Philippines', ' Italy', ' Poland',
+                                        ' Columbia', ' Cambodia', ' Thailand', ' Ecuador', ' Laos',
+                                        ' Taiwan', ' Haiti', ' Portugal', ' Dominican-Republic',
+                                        ' El-Salvador', ' France', ' Guatemala', ' China', ' Japan',
+                                        ' Yugoslavia', ' Peru', ' Outlying-US(Guam-USVI-etc)', ' Scotland',
+                                        ' Trinadad&Tobago', ' Greece', ' Nicaragua', ' Vietnam', ' Hong',
+                                        ' Ireland', ' Hungary', ' Holand-Netherlands'])
         logging.info("The Streamlit input field creation done")
 
        
@@ -91,8 +96,8 @@ if __name__ == "__main__":
             predictions = prediction_pipeline.make_prediction(input_data)
 
             # Display the predictions
-            st.write("The predicated salary of the person is:")
-            st.write(predictions)
+            st.header("The predicted salary of the person is:")
+            st.header(predictions[0])
 
     except Exception as e:
         logging.info("Custom Exception")
